b48ec2dd87f428d171c5086ef709bec36ec87895
iordan93 <iordan93@gmail.com>
2015-09-18 02:16:34 +0300

    Created the initial project structure

---
+++ SiriusSyncr/SiriusSyncr.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiriusSyncr.Data
+{
+    public class Class1
+    {
+    }
+}
